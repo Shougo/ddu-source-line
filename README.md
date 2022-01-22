8afea4009dd8814d5d13d687037814d588f57a5f
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-01-22 14:04:54 +0900

    Initial version

---
+++ README.md
@@ -0,0 +1,26 @@
+# ddu-source-line
+
+Line source for ddu.vim
+
+This source collects current buffer lines.
+
+## Required
+
+### denops.vim
+
+https://github.com/vim-denops/denops.vim
+
+### ddu.vim
+
+https://github.com/Shougo/ddu.vim
+
+### ddu-kind-file
+
+https://github.com/Shougo/ddu-kind-file
+
+## Configuration
+
+```vim
+" Use line source.
+call ddu#start([{'name': 'line'}])
+```
---
+++ denops/@ddu-sources/line.ts
@@ -0,0 +1,37 @@
+import { BaseSource, Context, Item } from "https://deno.land/x/ddu_vim@v0.1.0/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v0.1.0/deps.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.1.0/file.ts#^";
+
+type Params = Record<never, never>;
+
+export class Source extends BaseSource<Params> {
+  kind = "file";
+
+  gather(args: {
+    denops: Denops;
+    context: Context;
+    sourceParams: Params;
+  }): ReadableStream<Item<ActionData>[]> {
+    return new ReadableStream({
+      async start(controller) {
+        const bufnr = args.context.bufNr;
+        const lines = await fn.getline(args.denops, 1, "$");
+        controller.enqueue(lines.map((line, i) => {
+          return {
+            word: line,
+            action: {
+              bufNr: bufnr,
+              lineNr: i + 1,
+            },
+          };
+        }));
+
+        controller.close();
+      },
+    });
+  }
+
+  params(): Params {
+    return {};
+  }
+}
